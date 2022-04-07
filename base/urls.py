412f42502e74659c6e5138f01f002926f1369af3
waadshaher <waadraggam23@gmail.com>
2022-04-07 10:40:44 +0800

    Delete task

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskCreate, TaskList, TaskUpdate
+from .views import TaskCreate, TaskList, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task-create/',TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -16,4 +16,8 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
