1e327450fc107ff697f550eef21d43f670e65494
waadshaher <waadraggam23@gmail.com>
2022-04-07 10:32:30 +0800

    Update task

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskCreate, TaskList
+from .views import TaskCreate, TaskList, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task-create/',TaskCreate.as_view(), name = 'task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -10,6 +10,10 @@ class TaskList(ListView):
     context_object_name = 'tasks'
 
 class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
